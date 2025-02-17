444859bb261f9036c8fc85118b02b2ff935a9ee2
ComfortCityBud <gleb@gmail.com>
2025-02-17 14:39:21 +0200

    solution

--- user/serializers.py
+++ user/serializers.py
@@ -8,8 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ("id", "username", "email", "password", "is_staff")
         read_only_fields = ("id", "is_staff")
         extra_kwargs = {
-            "password": {"write_only": True},
-            "max_length": {"min_value": 3, "max_value": 20},
+            "password": {"write_only": True, "min_length": 5},
         }
 
     def create(self, validated_data):
